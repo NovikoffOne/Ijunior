728cf733a29b299670742237357f57c46fdb161b
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-20 21:09:50 +0300

    Add files via upload

---
+++ dzVar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dzVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 27;
+            string myName = "Ivan";
+            float Pi = 3.14f;
+            long distSun = 149597870700;
+            char questionMark = '?';
+            bool isDead = false;
+            uint countChair = 10;
+            ushort countByte = 2;
+            byte countRibs = 24;
+            short meltingPointOfLead = 327;
+
+
+        }
+    }
+}
