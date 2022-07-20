2c0ca804c413ed722bda8829c2214d5bcbb793dd
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-20 21:53:07 +0300

    Add files via upload

---
+++ Pictures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int rowPictures = 3;
+            int remainderPictures = allPictures % rowPictures;
+            int printRow = allPictures / rowPictures;
+
+            Console.WriteLine($"Количество заполненых рядов {printRow}");
+            Console.WriteLine($"Картинок сверх меры : {remainderPictures}");
+        }
+    }
+}
