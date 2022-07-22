0446bb94fc6f91caa2640ae98c3c493aab6b4e27
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-22 20:23:48 +0300

    Add files via upload

---
+++ queueAtTheClinic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queueAtTheClinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int appointmentTimePerPerson = 10;
+            int minutesInAnHour = 60;
+
+            Console.Write("Сколько людей перед вами в очереди? : ");
+            int amountOfPeopleInLine = Convert.ToInt32(Console.ReadLine());
+ 
+            int waitingTime = amountOfPeopleInLine * appointmentTimePerPerson;
+            int waitForHours = waitingTime / minutesInAnHour;
+            int waitMinutes = waitingTime % minutesInAnHour;
+
+            Console.WriteLine($"Время ожидания в очереди {waitForHours} ч {waitMinutes} м.");
+        }
+    }
+}
