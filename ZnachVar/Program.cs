fc6cc4df73a0ef6e0ff9bccae9d36a4279ff2dd2
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-22 20:27:55 +0300

    Add files via upload

---
+++ ZnachVar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZnachVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kozlov";
+            string surname = "Vlad";
+            string auxiliaryVariable;
+
+            Console.WriteLine($"Имя - {name}, Фамилия - {surname}.");
+
+            auxiliaryVariable = name;
+            name = surname;
+            surname = auxiliaryVariable;
+
+            Console.WriteLine($"Имя - {name}, Фамилия - {surname}.");
+        }
+    }
+}
