545ec95f01a74f792c0888bf3a69a0684f0e69e2
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-24 19:28:45 +0300

    Add files via upload

---
+++ cycleFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cycleFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopNumber = 100;
+            int stepNumber = 7;
+
+            for (int startNumber = 5; startNumber < stopNumber; startNumber += stepNumber)
+            {
+                Console.WriteLine(startNumber);
+            }
+        }
+    }
+}
