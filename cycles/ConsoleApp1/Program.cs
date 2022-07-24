754c7514284d8cf025124c2d51b489bbe8681e01
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-24 19:34:45 +0300

    Add files via upload

---
+++ cycles/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int amountLine;
+
+            Console.Write("Введите текст : ");
+            message = Console.ReadLine();
+
+            Console.Write("Введите количество : ");
+            amountLine = Convert.ToInt32(Console.ReadLine());
+
+            for (int count = 0; count < amountLine; count++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
