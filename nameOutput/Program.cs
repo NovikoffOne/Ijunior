3a7baa54d9c408b4d6ade6cc5194eef06cf81a41
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-07-27 19:57:03 +0300

    Add files via upload

---
+++ nameOutput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Переменные
+            string userName;
+            char userChar;
+            int lengthCharLine;
+
+            // Ввод
+            Console.Write("Добрый день, как вас зовут : ");
+            userName = Console.ReadLine();
+
+            Console.Write("Введите символ : ");
+            userChar = Convert.ToChar(Console.ReadLine());
+
+            // Форматирование строк с символами
+            lengthCharLine = (userName.Length) + 2;
+
+            // Вывод
+            Console.WriteLine(new string(userChar, lengthCharLine));
+            Console.WriteLine(userChar + userName + userChar);
+            Console.WriteLine(new string(userChar, lengthCharLine));
+        }
+    }
+}
