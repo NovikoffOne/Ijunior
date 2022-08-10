a0f5e8d17dd4870de9e79c7d8480bbcdea32663d
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-10 19:49:59 +0300

    Add files via upload

---
+++ workingWithStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberArray = { { 6, 3, 5, 7, 9 }, { 2, 4, 6, 8, 0 } };
+            int sumLine = 0;
+            int summedString = 1;
+            int сolumnsMultiplication = 1;
+
+            for (int i = 0; i < numberArray.GetLength(1); i++)
+            {
+                sumLine += numberArray[summedString, i];
+            }
+            
+            сolumnsMultiplication = numberArray[0, 0] * numberArray[1, 0];
+
+            for (int i = 0; i < numberArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numberArray.GetLength(1); j++)
+                {
+                    Console.Write(numberArray[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine(sumLine + " Сумма второй строки");
+            Console.WriteLine(сolumnsMultiplication + " Произведение первого столбца");
+        }
+    }
+}
