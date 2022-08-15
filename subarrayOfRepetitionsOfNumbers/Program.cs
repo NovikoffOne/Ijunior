efe2ce708d51f43bd20eba5fa1eef46cb83b988e
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-15 21:18:22 +0300

    Add files via upload

---
+++ subarrayOfRepetitionsOfNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subarrayOfRepetitionsOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumber = new int[30];
+            int[] arrayNumberRepetition = new int[0];
+            int tempNumber;
+            int countRepetition;
+            Random random = new Random();
+            
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                arrayNumber[i] = random.Next(1, 5);
+            }
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                Console.Write(arrayNumber[i] + " ");
+            }
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                tempNumber = arrayNumber[i];
+                for (int j = 0; tempNumber == arrayNumber[i]; j++)
+                {
+                    arrayNumberRepetition[j] = arrayNumber[i];
+                    Console.Write(arrayNumberRepetition[j] + " ");
+                }
+            }
+        }
+    }
+}
