ab92939e6ba22e769a96ea9e1ec5677e8be8c36c
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-15 20:29:28 +0300

    Add files via upload

---
+++ dynamicArray/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string userInput;
+            int userNumber;
+            int sum;
+            int[] arrayNumber = new int[0];
+
+            while (isWork)
+            {
+                Console.SetCursorPosition(0, 20);
+                Console.WriteLine("Введите число, что бы добавить его в массив.");
+                Console.WriteLine("Введите \"sum\" чтобы получить сумму всех чисел в массиве.");
+                Console.WriteLine("Введите \"exit\" для выхода.");
+                Console.SetCursorPosition(0, 0);
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "sum":
+                        sum = 0;
+                        
+                        for (int i = 0; i < arrayNumber.Length; i++)
+                        {
+                            sum += arrayNumber[i];
+                        }
+
+                        Console.WriteLine("Сумма равна - " + sum);
+                        Console.ReadKey();
+                        break;
+
+                    case "exit":
+                        Console.WriteLine("Увидимся)");
+                        isWork = false;
+                        break;
+
+                    default:
+                        userNumber = Convert.ToInt32(userInput);
+                        int[] tempArrayNumber = new int[arrayNumber.Length + 1];
+                        
+                        for (int i = 0; i < arrayNumber.Length; i++)
+                        {
+                            tempArrayNumber[i] = arrayNumber[i];
+                        }
+                        tempArrayNumber[tempArrayNumber.Length - 1] = userNumber;
+                        arrayNumber = tempArrayNumber;
+                        break;
+                }
+                
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ dynamicArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("dynamicArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dynamicArray")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("47c0694b-7e74-4c71-9f3f-9a7463329830")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
