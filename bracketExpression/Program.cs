24b9644623f2e6e0627e95d918684549a5cc1808
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-16 19:53:44 +0300

    Add files via upload

---
+++ bracketExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bracketExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bracketExpression = "(((((((()";
+            bool expressionCorrect = false;
+            float bracketTemp = 0;
+
+            for (int i = 0; i < bracketExpression.Length; i++)
+            {
+                if (bracketExpression[i] == "(")
+                {
+                    bracketTemp += 0.5f;
+                }
+                
+                //Console.WriteLine(bracketExpression[i]);
+                //if (bracketExpression == ")")
+                //{
+                //    expressionCorrect = false;
+                //}
+                //else
+                //    expressionCorrect = true;
+            }
+            
+            Console.WriteLine(expressionCorrect);
+        }
+    }
+}
