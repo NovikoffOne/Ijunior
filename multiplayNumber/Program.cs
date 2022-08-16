df8beb55cb154cdb0066613c3221a7e1bf5bd2b9
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-16 19:25:42 +0300

    Add files via upload

---
+++ multiplayNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplayNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 27);
+            int numberMultiplay = 0;
+            int countMultiplay = 0;
+
+            for (int i = 0; numberMultiplay < 1000; i++)
+            {
+                numberMultiplay += number;
+                
+                if (numberMultiplay > 100)
+                {
+                    countMultiplay += 1;
+                }
+            }
+
+            Console.WriteLine("N равно - " + number);
+            Console.WriteLine("Кол-во трехзначных натуральных чисел кратных N, равно - " + countMultiplay);
+        }
+    }
+}
