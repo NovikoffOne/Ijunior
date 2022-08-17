5451a6bca25a9b454cae75390efc32bc06a5ecf1
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-17 21:03:31 +0300

    Add files via upload

---
+++ split/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomText = "Привет как дела так давно не виделись";
+            char separetion = ' ';
+            string[] wordArray = randomText.Split(separetion);
+            
+            foreach (string word in wordArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
