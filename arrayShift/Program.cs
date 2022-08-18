50e6a446754917db727e693e25a85a322b2de6ee
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-18 19:31:11 +0300

    Add files via upload

---
+++ arrayShift/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayShift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumber = new int[] { 1, 2, 3, 4, 5, 6 };
+            int userInput;
+            
+            userInput = Convert.ToInt32(Console.ReadLine());
+            
+            for (int count = 0; userInput > count; count++ )
+            {
+                int firstNumberArray;
+                firstNumberArray = arrayNumber[0];
+                
+                for (int i = 0; i < arrayNumber.Length - 1; i++)
+                {
+                    arrayNumber[i] = arrayNumber[i + 1];
+                }
+                
+                arrayNumber[arrayNumber.Length - 1] = firstNumberArray;
+            }
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                Console.Write(arrayNumber[i] + " ");
+            }
+        }
+    }
+}
