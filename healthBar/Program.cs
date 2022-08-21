8cea0f46a3510145cfe84a338360f256d5fb8441
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-21 20:34:09 +0300

    Add files via upload

---
+++ healthBar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+            int maxValue = 10;
+            DrawBar(value, maxValue);
+        }
+
+        static void DrawBar(int value, int maxValue, ConsoleColor color = ConsoleColor.Red)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+
+            Console.SetCursorPosition(0, 0);
+            Console.BackgroundColor = color;
+            Console.Write("[");
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write('#');
+            }
+            Console.BackgroundColor = defaultColor;
+            Console.SetCursorPosition(value, 0);
+            for (int i = value; i < maxValue; i++)
+            {
+                if (i != maxValue - 1)
+                {
+                    Console.Write(' ');
+                }
+                else
+                {
+                    Console.Write(" " + ']');
+                }
+                
+            }
+
+        }
+    }
+}
