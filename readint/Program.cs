cf4e2d4db4e536620748c0cbf5242f2f9da5ede7
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-23 20:03:55 +0300

    Add files via upload

---
+++ readint/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int convertNumber = TryParse();
+        }
+
+        static int TryParse()
+        {
+            bool succes = false;
+            int number = 0;
+
+            while (succes != true)
+            {
+                int tempNumber;
+
+                Console.Write("Введите элемент для конвертации : ");
+                string value = Console.ReadLine();
+
+                succes = int.TryParse(value, out tempNumber);
+
+                if (succes == true)
+                {
+                    Console.WriteLine("Успех - " + tempNumber);
+                    number = tempNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Не удалось, попробуйте еще раз!");
+                }
+            }
+
+            return number;
+        }
+    }
+}
