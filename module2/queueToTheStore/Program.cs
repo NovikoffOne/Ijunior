567a11c4ca8854710d4a4cf8a903df6e738b485c
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-08-29 20:40:49 +0300

    Add files via upload

---
+++ module2/queueToTheStore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queueToTheStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int check = 0;
+            int[] numbers = { 1699, 4999, 2499, 1999, 6899 };
+            Queue<int> purchaseAmount = new Queue<int>(numbers);
+
+            ServeAClient(check, purchaseAmount);
+        }
+
+        static void ServeAClient(int check, Queue<int> shoppingList)
+        {
+            for (int i = shoppingList.Count; i > 0; i--)
+            {
+                check += shoppingList.Dequeue();
+                Console.WriteLine("Счет - " + check);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
