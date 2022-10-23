687f099f19ace779d71b42934d8cb9f697f77ab5
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-10-23 09:50:04 +0300

    Add files via upload

---
+++ module2/ArmamentCheck/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmamentCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Soldier> soldiers = new List<Soldier>() 
+            {
+                new Soldier("Петр", "АК-74", "Рядовой", 4),
+                new Soldier("Игорь", "АК-74", "Майор", 20),
+                new Soldier("Афанасий", "ПП", "Генерал", 50),
+                new Soldier("Олег", "АК-74", "Сержант", 8),
+                new Soldier("Артем", "Миномет", "Лейтенант", 15),
+                new Soldier("Дмитрий", "Сайга", "Рядовой", 2),
+                new Soldier("Василий", "АКМ", "Сержант", 9),
+            };
+
+            var newSoldier = from Soldier soldier in soldiers select new { Name = soldier.Name, Rank = soldier.Rank };
+
+            foreach(var soldier in newSoldier)
+            {
+                Console.WriteLine($"{soldier.Name} - {soldier.Rank}");
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public string Name { get; private set; }
+        public string Weapon { get; private set; }
+        public string Rank { get; private set; }
+        public int LifeTime { get; private set; }
+
+        public Soldier(string name, string weapon, string rank, int lifeTime)
+        {
+            Name = name;
+            Weapon = weapon;
+            Rank = rank;
+            LifeTime = lifeTime;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} | {Rank} | {Weapon} | {LifeTime}");
+        }
+    }
+}
---
+++ module2/ArmamentCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ArmamentCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArmamentCheck")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9e96e17a-b0b7-4e94-a7e5-c860b2efd72c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
