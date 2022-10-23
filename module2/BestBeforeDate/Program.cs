ce54de93de11435e9bb09dd0f398e89220623817
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2022-10-23 09:27:59 +0300

    Add files via upload

---
+++ module2/BestBeforeDate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestBeforeDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Stew> stews = new List<Stew>() {
+                new Stew("Говяжая", 2019, 5),
+                new Stew("Свинная", 2013, 7),
+                new Stew("Куринная", 2021, 9),
+                new Stew("Буренка", 2016, 4),
+                new Stew("Вкусняка", 2015, 5),
+                new Stew("Свежак", 2019, 1),
+                new Stew("Походная", 2008, 10),
+            };
+
+            DateTime toDay = DateTime.Today;
+
+            var filtredStew = stews.Where(stew => stew.SellBy > toDay);
+
+            foreach(var stew in filtredStew)
+            {
+                stew.ShowInfo();
+            }
+        }
+    }
+
+    class Stew
+    {
+        private string _name;
+        private int _bestBeforeDate;
+        private DateTime _dateOfManufacture = new DateTime();
+
+        public DateTime SellBy { get; private set; } = new DateTime();
+
+        public Stew(string name, int dateOfManufacture, int bestBeforeDate)
+        {
+            _name = name;
+            _dateOfManufacture = _dateOfManufacture.AddYears(dateOfManufacture);
+            _bestBeforeDate = bestBeforeDate;
+            SellBy = _dateOfManufacture.AddYears(_bestBeforeDate);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{_name} | Произведенно : {_dateOfManufacture.ToString("yyyy")} | Срок годности : {_bestBeforeDate}");
+        }
+    }
+}
---
+++ module2/BestBeforeDate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BestBeforeDate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BestBeforeDate")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("369587ea-58f3-4f5a-a759-133eed0a696e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
