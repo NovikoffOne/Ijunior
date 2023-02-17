f03148f11d760084845e7f83637ffef79efb3ef3
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-02-17 20:14:23 +0300

    Commit 2

--- crystalStore/Program.cs
+++ crystalStore/Program.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace crystalStore
+namespace CurrencyConverter2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int courseOfCrystals = 10;
+            int pricePerCrystal = 10;
 
             Console.WriteLine("Добро пожаловть в магазин кристаллов! Курс равен 1:10 ");
 
             Console.Write("Сколько у вас золота : ");
-            int goldInInventory = Convert.ToInt32(Console.ReadLine());
+            int gold = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Сколько кристаллов вы хотите приобрести? : ");
-            int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
+            int crystalsQuantity = Convert.ToInt32(Console.ReadLine());
 
-            int crystalsInInventory = purchasedCrystals; 
-            goldInInventory -= (purchasedCrystals * courseOfCrystals);
+            int crystalsInInventory = crystalsQuantity;
+            gold -= crystalsQuantity * pricePerCrystal;
 
-            Console.WriteLine($"В вашем инвентаре {crystalsInInventory} кристаллов и {goldInInventory}");
+            Console.WriteLine($"В вашем инвентаре {crystalsInInventory} кристаллов и {gold}");
         }
     }
 }
